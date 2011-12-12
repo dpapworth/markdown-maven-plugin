9afe2ad33f7e89f18f2452de7de05674651d31b3
Json Shen <json.shen@gmail.com>
2011-12-13 00:22:39 +0800

    代码格式化

--- src/main/java/org/shenjia/maven/markdown/MarkdownMojo.java
+++ src/main/java/org/shenjia/maven/markdown/MarkdownMojo.java
@@ -40,181 +40,195 @@ import com.petebevin.markdown.MarkdownProcessor;
 
 /**
  * Markdown maven plugin mojo
+ * 
  * @author json.shen@gmail.com (Json Shen)
  * @goal markdown
  * @phase process-sources
  */
 public class MarkdownMojo extends AbstractMojo {
-	
-	/**
-	 * markdown文件根目录
+
+    /**
+     * markdown文件根目录
+     * 
      * @parameter expression="${markdown.directory}"
      * @required
      */
     private File markdownDirectory;
-    
-	/**
-	 * 输出文件根目录
-	 * @parameter expression="${project.build.directory}"
-	 * @required
-	 */
-	private File outputDirectory;
-	
-	/**
-	 * 目标文件头
-	 * @parameter expression="${html.header}" default-value="<!DOCTYPE html><head><title>{title}</title></head><body>\n"
-	 */
-	private String header;
-	
-	/**
-	 * 目标文件尾
-	 * @parameter expression="${html.footer}" default-value="</body></html>"
-	 */
-	private String footer ;
-	
-	/**
-	 * 是否复制markdown文件到输出目录
+
+    /**
+     * 输出文件根目录
+     * 
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File outputDirectory;
+
+    /**
+     * 目标文件头
+     * 
+     * @parameter expression="${html.header}" default-value="<!DOCTYPE html><head><title>{title}</title></head><body>\n"
+     */
+    private String header;
+
+    /**
+     * 目标文件尾
+     * 
+     * @parameter expression="${html.footer}" default-value="</body></html>"
+     */
+    private String footer;
+
+    /**
+     * 是否复制markdown文件到输出目录
+     * 
      * @parameter expression="${copy.markdown}" default-value="true"
      */
     private Boolean copyMarkdown;
-	
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		if(!outputDirectory.exists()){
-			outputDirectory.mkdirs();
-		}
-		//递归处理markdown
-		processMarkdown(markdownDirectory, new MarkdownProcessor());
-	}
-	
-	private void processMarkdown(File file, MarkdownProcessor processor){
-		//markdown文件根目录路径长度
-		int srcDirPathLen = markdownDirectory.getPath().length();
-		File[] files = file.listFiles(createFileFilter());
-		for(File f : files){
-			if(f.isDirectory()){
-				processMarkdown(f,processor);
-			}else{
-				BufferedReader in = null;
-				BufferedWriter out = null;
-				try {
-					//输出目录
-					File outputDir = outputDirectory;
-					String subPath = f.getParentFile().getPath().substring(srcDirPathLen);
-					if(subPath.length()>0){
-						outputDir = new File(outputDirectory, subPath);
-						if(!outputDir.exists()){
-							outputDir.mkdirs();
-						}
-					}
-					//输出文件名
-					String outputName = f.getName().replaceAll("\\.md", ".html");
-					
-					//输出流，用于写文件
-					out = new BufferedWriter(new FileWriter(new File(outputDir, outputName)));
-					//输出流，用于读文件
-					in = new BufferedReader(new FileReader(f));
-					
-					String outLine = in.readLine();
-					String outTag = getLineTag(outLine);
-					//写入目标文件头
-					out.write(header.replaceFirst("\\{title}", outLine));
-					
-					String logicLine = null;
-					String logicTag = null;
-					
-					while(outLine != null){
-						if(outTag.equals("h1") || outTag.equals("h2")){
-							out.write(processor.markdown(outLine));
-							outLine = in.readLine();
-							outTag = getLineTag(outLine);
-							continue;
-						}
-						
-						logicLine = in.readLine();
-						if(logicLine == null){
-							out.write(processor.markdown(outLine));
-							break;
-						}
-						
-						logicTag = getLineTag(logicLine);
-						if(logicTag.equals("")){
-							out.write(processor.markdown(outLine));
-							outLine = logicLine;
-							outTag = getLineTag(outLine);
-							continue;
-						}
-						
-						if(logicTag.equals("h1") || logicTag.equals("h2")){
-							out.write(processor.markdown(outLine+"\n"+logicLine));
-							outLine = in.readLine();
-							outTag = getLineTag(outLine);
-							continue;
-						}
-						
-						if(logicTag.equals("ul") || logicTag.equals("ol")){
-							if(logicTag.equals(outTag)){
-								outLine = outLine + "\n" + logicLine;
-								continue;
-							}
-							out.write(processor.markdown(outLine));
-							outLine = logicLine;
-							outTag = getLineTag(outLine);
-							continue;
-						}
-					}
-					//写入目标文件尾
-					out.write(footer);
-				} catch (FileNotFoundException e) {
-					getLog().error(e.getMessage(),e);
-				} catch (IOException e) {
-					getLog().error(e.getMessage(),e);
-				} finally {
-					try {
-						if(in != null)in.close();
-						if(out != null)out.close();
-					} catch (IOException e) {
-						getLog().error(e.getMessage(),e);
-					}
-				}
-			}
-		}
-	}
-	
-	private String getLineTag(String line){
-		if(line == null){
-			return "";
-		}
-		if(line.startsWith("*") || line.startsWith("+") || line.startsWith("-")){
-			return "ul";
-		}
-		if(line.startsWith("====")){
-			return "h1";
-		}
-		if(line.startsWith("----")){
-			return "h2";
-		}
-		if(line.startsWith("[0-9]+")){
-			return "ol";
-		}
-		return "";
-	}
-	
-	/**
-	 * 创建一个文件过滤器
-	 * @return
-	 */
-	private FileFilter createFileFilter(){
-		return new FileFilter() {
-			public boolean accept(File file) {
-				if(file.isDirectory()){
-					return true;
-				}
-				if(file.getName().endsWith(".md")){
-					return true;
-				}
-				return false;
-			}
-		};
-	}
-	
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
+        // 递归处理markdown
+        processMarkdown(markdownDirectory, new MarkdownProcessor());
+    }
+
+    private void processMarkdown(File file, MarkdownProcessor processor) {
+        // markdown文件根目录路径长度
+        int srcDirPathLen = markdownDirectory.getPath().length();
+        File[] files = file.listFiles(createFileFilter());
+        for (File f : files) {
+            if (f.isDirectory()) {
+                processMarkdown(f, processor);
+            } else {
+                BufferedReader in = null;
+                BufferedWriter out = null;
+                try {
+                    // 输出目录
+                    File outputDir = outputDirectory;
+                    String subPath = f.getParentFile().getPath()
+                            .substring(srcDirPathLen);
+                    if (subPath.length() > 0) {
+                        outputDir = new File(outputDirectory, subPath);
+                        if (!outputDir.exists()) {
+                            outputDir.mkdirs();
+                        }
+                    }
+                    // 输出文件名
+                    String outputName = f.getName()
+                            .replaceAll("\\.md", ".html");
+
+                    // 输出流，用于写文件
+                    out = new BufferedWriter(new FileWriter(new File(outputDir,
+                            outputName)));
+                    // 输出流，用于读文件
+                    in = new BufferedReader(new FileReader(f));
+
+                    String outLine = in.readLine();
+                    String outTag = getLineTag(outLine);
+                    // 写入目标文件头
+                    out.write(header.replaceFirst("\\{title}", outLine));
+
+                    String logicLine = null;
+                    String logicTag = null;
+
+                    while (outLine != null) {
+                        if (outTag.equals("h1") || outTag.equals("h2")) {
+                            out.write(processor.markdown(outLine));
+                            outLine = in.readLine();
+                            outTag = getLineTag(outLine);
+                            continue;
+                        }
+
+                        logicLine = in.readLine();
+                        if (logicLine == null) {
+                            out.write(processor.markdown(outLine));
+                            break;
+                        }
+
+                        logicTag = getLineTag(logicLine);
+                        if (logicTag.equals("")) {
+                            out.write(processor.markdown(outLine));
+                            outLine = logicLine;
+                            outTag = getLineTag(outLine);
+                            continue;
+                        }
+
+                        if (logicTag.equals("h1") || logicTag.equals("h2")) {
+                            out.write(processor.markdown(outLine + "\n"
+                                    + logicLine));
+                            outLine = in.readLine();
+                            outTag = getLineTag(outLine);
+                            continue;
+                        }
+
+                        if (logicTag.equals("ul") || logicTag.equals("ol")) {
+                            if (logicTag.equals(outTag)) {
+                                outLine = outLine + "\n" + logicLine;
+                                continue;
+                            }
+                            out.write(processor.markdown(outLine));
+                            outLine = logicLine;
+                            outTag = getLineTag(outLine);
+                            continue;
+                        }
+                    }
+                    // 写入目标文件尾
+                    out.write(footer);
+                } catch (FileNotFoundException e) {
+                    getLog().error(e.getMessage(), e);
+                } catch (IOException e) {
+                    getLog().error(e.getMessage(), e);
+                } finally {
+                    try {
+                        if (in != null)
+                            in.close();
+                        if (out != null)
+                            out.close();
+                    } catch (IOException e) {
+                        getLog().error(e.getMessage(), e);
+                    }
+                }
+            }
+        }
+    }
+
+    private String getLineTag(String line) {
+        if (line == null) {
+            return "";
+        }
+        if (line.startsWith("*") || line.startsWith("+")
+                || line.startsWith("-")) {
+            return "ul";
+        }
+        if (line.startsWith("====")) {
+            return "h1";
+        }
+        if (line.startsWith("----")) {
+            return "h2";
+        }
+        if (line.startsWith("[0-9]+")) {
+            return "ol";
+        }
+        return "";
+    }
+
+    /**
+     * 创建一个文件过滤器
+     * 
+     * @return
+     */
+    private FileFilter createFileFilter() {
+        return new FileFilter() {
+            public boolean accept(File file) {
+                if (file.isDirectory()) {
+                    return true;
+                }
+                if (file.getName().endsWith(".md")) {
+                    return true;
+                }
+                return false;
+            }
+        };
+    }
+
 }
