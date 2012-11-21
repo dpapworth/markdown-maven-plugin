b86f1f564b3e26213b9f229f7fd5d328a995529b
Damien Papworth <damien.papworth@vibrantmedia.com>
2012-11-21 11:39:28 -0500

    Replaced markdownj with pegdown.

--- src/main/java/org/shenjia/maven/markdown/MarkdownMojo.java
+++ src/main/java/org/shenjia/maven/markdown/MarkdownMojo.java
@@ -23,20 +23,15 @@
 
 package org.shenjia.maven.markdown;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.parboiled.common.FileUtils;
+import org.pegdown.PegDownProcessor;
 
-import com.petebevin.markdown.MarkdownProcessor;
 
 /**
  * Markdown maven plugin mojo
@@ -82,127 +77,34 @@ public class MarkdownMojo extends AbstractMojo {
             outputDirectory.mkdirs();
         }
         // 递归处理markdown
-        processMarkdown(sourceDirectory, new MarkdownProcessor());
+        processMarkdown(sourceDirectory, new PegDownProcessor());
     }
 
-    private void processMarkdown(File file, MarkdownProcessor processor) {
+    private void processMarkdown(File file, PegDownProcessor processor) throws MojoExecutionException {
         // markdown文件根目录路径长度
         int srcDirPathLen = sourceDirectory.getPath().length();
         File[] files = file.listFiles(createFileFilter());
-        for (File f : files) {
-            if (f.isDirectory()) {
-                processMarkdown(f, processor);
+        for (File inputFile : files) {
+            if (inputFile.isDirectory()) {
+                processMarkdown(inputFile, processor);
             } else {
-                BufferedReader in = null;
-                BufferedWriter out = null;
-                try {
-                    // 输出目录
-                    File outputDir = outputDirectory;
-                    String subPath = f.getParentFile().getPath()
-                            .substring(srcDirPathLen);
-                    if (subPath.length() > 0) {
-                        outputDir = new File(outputDirectory, subPath);
-                        if (!outputDir.exists()) {
-                            outputDir.mkdirs();
-                        }
-                    }
-                    // 输出文件名
-                    String outputName = f.getName()
-                            .replaceAll("\\.md", ".html");
-
-                    // 输出流，用于写文件
-                    out = new BufferedWriter(new FileWriter(new File(outputDir,
-                            outputName)));
-                    // 输出流，用于读文件
-                    in = new BufferedReader(new FileReader(f));
-
-                    String outLine = in.readLine();
-                    String outTag = getLineTag(outLine);
-                    // 写入目标文件头
-                    out.write(htmlHeader.replaceFirst("\\{title}", outLine));
-
-                    String logicLine = null;
-                    String logicTag = null;
-
-                    while (outLine != null) {
-                        if (outTag.equals("h1") || outTag.equals("h2")) {
-                            out.write(processor.markdown(outLine));
-                            outLine = in.readLine();
-                            outTag = getLineTag(outLine);
-                            continue;
-                        }
-
-                        logicLine = in.readLine();
-                        if (logicLine == null) {
-                            out.write(processor.markdown(outLine));
-                            break;
-                        }
-
-                        logicTag = getLineTag(logicLine);
-                        if (logicTag.equals("")) {
-                            out.write(processor.markdown(outLine));
-                            outLine = logicLine;
-                            outTag = getLineTag(outLine);
-                            continue;
-                        }
-
-                        if (logicTag.equals("h1") || logicTag.equals("h2")) {
-                            out.write(processor.markdown(outLine + "\n"
-                                    + logicLine));
-                            outLine = in.readLine();
-                            outTag = getLineTag(outLine);
-                            continue;
-                        }
-
-                        if (logicTag.equals("ul") || logicTag.equals("ol")) {
-                            if (logicTag.equals(outTag)) {
-                                outLine = outLine + "\n" + logicLine;
-                                continue;
-                            }
-                            out.write(processor.markdown(outLine));
-                            outLine = logicLine;
-                            outTag = getLineTag(outLine);
-                            continue;
-                        }
-                    }
-                    // 写入目标文件尾
-                    out.write(htmlFooter);
-                } catch (FileNotFoundException e) {
-                    getLog().error(e.getMessage(), e);
-                } catch (IOException e) {
-                    getLog().error(e.getMessage(), e);
-                } finally {
-                    try {
-                        if (in != null)
-                            in.close();
-                        if (out != null)
-                            out.close();
-                    } catch (IOException e) {
-                        getLog().error(e.getMessage(), e);
+                File outputDir = outputDirectory;
+                String subPath = inputFile.getParentFile().getPath().substring(srcDirPathLen);
+                if (subPath.length() > 0) {
+                    outputDir = new File(outputDirectory, subPath);
+                    if (!outputDir.isDirectory() && !outputDir.mkdirs()) {
+                        throw new MojoExecutionException("Failed to create " + outputDir.getName());
                     }
                 }
+
+                String outputName = inputFile.getName().replaceAll("\\.md", ".html");
+                File outputFile = new File(outputDir, outputName);
+
+                String markdown = FileUtils.readAllText(inputFile);
+                String htmlFragment = processor.markdownToHtml(markdown);
+                FileUtils.writeAllText(htmlHeader + htmlFragment + htmlFooter, outputFile);
             }
         }
-    }
-
-    private String getLineTag(String line) {
-        if (line == null) {
-            return "";
-        }
-        if (line.startsWith("*") || line.startsWith("+")
-                || line.startsWith("-")) {
-            return "ul";
-        }
-        if (line.startsWith("====")) {
-            return "h1";
-        }
-        if (line.startsWith("----")) {
-            return "h2";
-        }
-        if (line.startsWith("[0-9]+")) {
-            return "ol";
-        }
-        return "";
     }
 
     /**
